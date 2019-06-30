7900f46304a103a405b20668b68b94f0659104dc
Yi HAN <hanyi@8110@gmail.com>
2019-06-30 20:35:42 +0200

    Add unit test: connect to bank fails twice then succeeds for the third time

--- src/PaymentGateway/Tests/PaymentGateway.Tests/RequestPaymentShould.cs
+++ src/PaymentGateway/Tests/PaymentGateway.Tests/RequestPaymentShould.cs
@@ -62,7 +62,7 @@ namespace PaymentGateway.Tests
 
         [TestCase(AcquiringBanks.API.BankPaymentStatus.Accepted, PaymentGateway.Domain.PaymentStatus.Success)]
         [TestCase(AcquiringBanks.API.BankPaymentStatus.Rejected, PaymentGateway.Domain.PaymentStatus.RejectedByBank)]
-        public async Task Return_proper_payment_status_When_AcquiringBank_accepts_payment(BankPaymentStatus bankPaymentStatus, PaymentStatus expectedPaymentStatusReturnedByGateway)
+        public async Task Return_proper_payment_status_When_AcquiringBank_accepts_or_reject_payment(BankPaymentStatus bankPaymentStatus, PaymentStatus expectedPaymentStatusReturnedByGateway)
         {
             var requestId = Guid.NewGuid();
             var paymentRequest = Utils.BuildPaymentRequest(requestId);
@@ -99,6 +99,29 @@ namespace PaymentGateway.Tests
             Check.That(payment.GatewayPaymentId).IsEqualTo(gatewayPaymentId);
 
             Check.That(payment.Status).IsEqualTo(PaymentStatus.FaultedOnGateway);
+        }
+
+
+        [TestCase(AcquiringBanks.API.BankPaymentStatus.Accepted, PaymentGateway.Domain.PaymentStatus.Success)]
+        [TestCase(AcquiringBanks.API.BankPaymentStatus.Rejected, PaymentGateway.Domain.PaymentStatus.RejectedByBank)]
+        public async Task Return_proper_payment_status_When_Connect_to_bank_fails_twice_then_connected_AND_AcquiringBank_accepts_or_reject_payment(BankPaymentStatus bankPaymentStatus, PaymentStatus expectedPaymentStatusReturnedByGateway)
+        {
+            var requestId = Guid.NewGuid();
+            var paymentRequest = Utils.BuildPaymentRequest(requestId);
+            var gatewayPaymentId = Guid.NewGuid();
+            IGenerateGuid guidGenerator = new GuidGeneratorForTesting(gatewayPaymentId);
+
+            var bankPaymentId = Guid.Parse("3ec8c76c-7dc2-4769-96f8-7e0649ecdfc0");
+            var cqrs = await PaymentCQRS.Build(bankPaymentStatus, new BankPaymentIdGeneratorForTests(bankPaymentId), new FailTwiceBankThenSuccessConnectionBehavior());
+            await cqrs.RequestsController.ProceedPaymentRequest(paymentRequest, guidGenerator, cqrs.PaymentIdsMapping, cqrs.PaymentProcessor);
+
+
+            var payment = (await cqrs.PaymentReadController.GetPaymentInfo(gatewayPaymentId)).Value;
+            Check.That(payment.RequestId).IsEqualTo(requestId);
+            Check.That(payment.GatewayPaymentId).IsEqualTo(gatewayPaymentId);
+
+            Check.That(payment.Status).IsEqualTo(expectedPaymentStatusReturnedByGateway);
+            Check.That(payment.AcquiringBankPaymentId).IsEqualTo(bankPaymentId);
         }
 
         [Test]
@@ -167,4 +190,19 @@ namespace PaymentGateway.Tests
         }
     }
 
+    public class FailTwiceBankThenSuccessConnectionBehavior : IBankConnectionBehavior
+    {
+        private int _failed = 0;
+
+        public async Task<bool> Connect()
+        {
+            while (_failed <= 2)
+            {
+                _failed++;
+                throw new FailedConnectionToBankException();
+            }
+
+            return await Task.FromResult(true);
+        }
+    }
 }
